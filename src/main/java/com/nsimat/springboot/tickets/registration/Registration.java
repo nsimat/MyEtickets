3ea403c0bb9920ca9402b9784e674c248f2620fe
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2024-06-18 03:12:24 +0200

    Adding validation to a Registration

--- src/main/java/com/nsimat/springboot/tickets/registration/Registration.java
+++ src/main/java/com/nsimat/springboot/tickets/registration/Registration.java
@@ -1,8 +1,13 @@
 package com.nsimat.springboot.tickets.registration;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record Registration(
         Integer id,
+        @NotNull(message = "product id is required!")
         Integer productId,
         String ticketCode,
+        @NotBlank(message = "Attendee name is required!")
         String attendeeName) {
 }
