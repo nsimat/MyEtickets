98f5853838c235e170b946c7903159c6f63b00f0
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2024-06-18 03:18:31 +0200

    Updating Registration annotation for NoSQL MongoDB records

--- src/main/java/com/nsimat/springboot/tickets/registration/Registration.java
+++ src/main/java/com/nsimat/springboot/tickets/registration/Registration.java
@@ -2,9 +2,13 @@ package com.nsimat.springboot.tickets.registration;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document("registrations")
 public record Registration(
-        Integer id,
+        @Id
+        String id,
         @NotNull(message = "product id is required!")
         Integer productId,
         String ticketCode,
